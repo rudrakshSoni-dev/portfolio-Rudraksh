71529de37d0657294ce271100fd16536206aff41
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-20 00:01:51 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+     <p className="text-3xl underline">hello</p>
+    </>
+  );
+}
